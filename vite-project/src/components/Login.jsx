39c06db15f682b8cdeced107a78b0c90f6f916d9
KrzysztofPardel <pardelkrzysztof97@gmail.com>
2023-03-09 14:09:18 +0100

    Responsiveness finished

--- vite-project/src/components/Login.jsx
+++ vite-project/src/components/Login.jsx
@@ -3,6 +3,12 @@ import '/src/scss/Login.scss'
 import globe from './globe.json'
 import backpack from './backpack.json'
 import Lottie from 'lottie-react'
+const backpackStyle = {
+	// height: '70vh',
+	// width: '50vw',
+	margin: '2em',
+	// padding: '-1em',
+}
 const Login = () => {
 	return (
 		<>
@@ -12,8 +18,9 @@ const Login = () => {
 					<span className='welcome-middle'>Trip Planner App</span>
 					<span className='welcome-bottom'>Plan well . Enjoy more .</span>
 				</div>
-				<Lottie animationData={backpack} />
-
+				<div className='animation-backpack'>
+					<Lottie animationData={backpack} style={backpackStyle} />
+				</div>
 				<div className='box'>
 					<input className='name' type='text' placeholder='What is your name?'></input>
 					<input className='trip-date' type='date' placeholder='When are you planning to travel?'></input>
@@ -26,7 +33,7 @@ const Login = () => {
 				</div>
 				<p className='quote'>
 					"The world is a book, and those who don’t travel read only a page."
-					<p className='quote-author'></p>
+					<p className='quote-author'>St.Augustin</p>
 				</p>
 				<div className='background'></div>
 			</div>
